b818c9df794f6e7f1635cf1cdcda7bf80572bb6c
Robbie-Jay Dearling <Robbie-Jay.Dearling@lexisnexisrisk.com>
2022-11-24 08:32:37 +0000

    Install automation

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Remove the login information on shell startup
+touch $HOME/.hushlogin
+
+# Update packages and upgrade
+sudo apt update
+sudo apt upgrade
+
+# Install ZSH
+echo 'Installing oh-my-zsh'
+## Remove any existing config for oh-my-zsh
+rm -rf  $HOME/.oh-my-zsh
+
+curl --location https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Setup global gitignore
+ln -s $HOME/.dotfiles/shell/.global-gitignore $HOME/.global-gitignore
+git config --global core.excludesfiles $HOME/.global-gitignore
+
+#Symlink zsh prefs
+rm $HOME/.zshrc
+ln -s $HOME/.dotfiles/shell/.zshrc $HOME/.zshrc
+
+
+# Fix missing font characters (see https://github.com/robbyrussell/oh-my-zsh/issues/1906)
+cd ~/.oh-my-zsh/themes/
+git checkout d6a36b1 agnoster.zsh-theme
---
+++ shell/.zshrc
@@ -0,0 +1,8 @@
+ZSH=$HOME/.oh-my-zsh
+
+# Set the ZSH theme
+# Themes are found in ~./oh-my-zsh/themes
+ZSH_THEME="agnoster"
+
+# Hides the username in prompt
+DEFAULT_USER=`whoami`
