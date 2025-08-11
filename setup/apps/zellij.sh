33294f6cf193f804c6dc9bd88e516d7e34055a6b
Rob Dearling <robbie-jay.dearling@lexisnexisrisk.com>
2025-08-11 12:01:21 +0100

    Implement reusable WSL

---
+++ setup/apps/zellij.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /tmp
+wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+rm -rf zellij
+tar -xf zellij.tar.gz zellij
+sudo install zellij /usr/local/bin
+rm zellij.tar.gz zellij
+cd -
+
+mkdir -p ~/.config/zellij/themes
+[ ! -f "$HOME/.config/zellij/config.kdl" ] && cp ~/.local/wsl-setup/configs/zellij.kdl ~/.config/zellij/config.kdl
+cp ~/.local/wsl-setup/themes/tokyo-night/zellij.kdl ~/.config/zellij/themes/tokyo-night.kdl
---
+++ setup/configure-git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global pull.rebase true
+
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
